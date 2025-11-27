131bb71dc6a9b7871e35ce6ab2dfa3f6377b799b
HoPGoldy <hpgless@outlook.com>
2025-11-27 21:54:41 +0800

    feat: 启用 WAL 模式

--- packages/backend/src/modules/prisma/index.ts
+++ packages/backend/src/modules/prisma/index.ts
@@ -11,6 +11,18 @@ export class PrismaService extends PrismaClient {
   }
 
   async seed() {
+    try {
+      await this.$queryRaw`PRAGMA journal_mode = WAL;`;
+      console.log("[sqlite] WAL mode enabled");
+
+      const result = (await this.$queryRaw`PRAGMA auto_vacuum;`) as any[];
+      const status = result[0]?.auto_vacuum;
+
+      console.log(`[sqlite] auto_vacuum status: ${status}`);
+    } catch (error) {
+      console.error("Error setting WAL mode:", error);
+    }
+
     const result = await this.$transaction(async (tx) => {
       const userCount = await tx.user.count();
       if (userCount === 0) {
